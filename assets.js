d06c607c54ad0311d4d2331ff915658c21a3ccec
apablo <ap.alonso3086@gmail.comm>
2022-08-29 23:24:56 -0500

    29_08_22-11_23

--- assets.js
+++ assets.js
@@ -3,10 +3,27 @@ console.log(elemento);
 }
 
 function pintaElemento(div, texto, tipo){
+    impre('Id00 Pinta elemento');
     if(tipo="append"){
         $('#'+div).append(texto);
     }
     if(tipo="html"){
+        impre('id01 esto es htnml');
+        $('#titulo').html('Calculadora');
         $('#'+div).html(texto);
     }
 }
+
+function divGeneral(){
+    $('#bloqueGeneral').html('<div id="titulo" class="bloqueContenido"></div>');
+}
+
+function temaCurso(){
+    if(flagCurso==true){
+        $('#bloqueGeneral').addClass('bloqueGeneral');            
+    }
+}
+
+
+
+
--- script_dom01.js
+++ script_dom01.js
@@ -1,2 +1,13 @@
-impre('Hola');
-pintaElemento('bloqueGeneral');
+var flagCurso=true;
+$( document ).ready(function() {
+divGeneral();
+debugger
+
+pintaElemento('titulo', 'Calculadora', 'html');
+impre('estamos en el debugger y la bandera es: ' + flagCurso);
+debugger
+impre('estamos fuera del debugger y la bandera es: ' + flagCurso);
+temaCurso();
+impre('estamos en la ultima linea');
+
+});
